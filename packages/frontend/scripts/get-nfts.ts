fb34f62aa394ba5e64ab63956499aed3cb76c33d
aeither <36173828+aeither@users.noreply.github.com>
2024-10-19 09:49:48 +0300

    filter rentals

--- packages/frontend/scripts/get-nfts.ts
+++ packages/frontend/scripts/get-nfts.ts
@@ -7,30 +7,21 @@ const rpcUrl = getFullnodeUrl("testnet");
 const client = new SuiClient({ url: rpcUrl });
 
 // get coins owned by an address
-// replace <OWNER_ADDRESS> with actual address in the form of 0x123...
 async function main() {
 	const ownedObjects = await client.getOwnedObjects({
 		owner: "0x978110b86146faa562d72a12131c5dc5021dd96d88e715c79d14fb9df0406c41",
-        options: {
-            showContent: true,
-            showDisplay: true,
-            showType: true
-        }
-		// filter: {
-		//     Package
-		// }
+		options: {
+			showContent: true,
+			showDisplay: true,
+			showType: true,
+		},
+		filter: {
+			StructType:
+				"0x23541b1f99e1e459f78cf675fc2974a60355ac62a35289c7b59bc835adff785b::nft::Rental",
+		},
 	});
 	console.log(ownedObjects.data);
 	console.log("content: ", ownedObjects.data[1].data?.content);
 	console.log("display: ", ownedObjects.data[1].data?.display);
-
-	// if (ownedObjects.data[0].data) {
-	// 	const object = await client.getObject({
-	// 		id: ownedObjects.data[0].data.objectId,
-    //         options: {}
-	// 	});
-	// 	console.log("🚀 ~ main ~ object:", object)
-	// }
 }
-
 main();
